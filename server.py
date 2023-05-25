d638786c66bca30fd3cfc018e0ea760fc136aa97
kenneth-alt <kenneth.ugo15@gmail.com>
2023-05-24 21:50:00 -0600

    updates routes

--- server.py
+++ server.py
@@ -17,23 +17,40 @@ app = Flask(__name__)
 
 
 def get_lat_lon():
-    url = f'https://api.opencagedata.com/geocode/v1/json?q={CITY}&key={OPENCAGE_API_KEY}'
-    response = requests.get(url)
-    lat_lon_data = response.json()
-    
-    LAT = lat_lon_data['results'][0]['geometry']['lat']
-    LON = lat_lon_data['results'][0]['geometry']['lng']
-    return LAT, LON
-    
+    try:
+        url = f'https://api.opencagedata.com/geocode/v1/json?q={CITY}&key={OPENCAGE_API_KEY}'
+        response = requests.get(url)
+        response.raise_for_status()
+        lat_lon_data = response.json()
+        
+        LAT = lat_lon_data['results'][0]['geometry']['lat']
+        LON = lat_lon_data['results'][0]['geometry']['lng']
+        return LAT, LON
+    except requests.exceptions.HTTPError as err:
+        return f"Error: {err}"
+
+@app.route('/current')    
 def current_weather():
-    LAT, LON = get_lat_lon()
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={OPENWEATHER_API_KEY}")
-    weather_data = response.json()
-    return  weather_data
+    try:
+        LAT, LON = get_lat_lon()
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={OPENWEATHER_API_KEY}")
+        weather_data = response.json()
+        return  weather_data
+    except requests.exceptions.HTTPError as err:
+        return f"Error: {err}"
+
+@app.route('/forcast')
+def forcast():
+    try:
+        LAT, LON = get_lat_lon()
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={OPENWEATHER_API_KEY}")
+        forcast_data = response.json()
+        return  forcast_data
+    except requests.exceptions.HTTPError as err:
+        return f"Error: {err}"
    
 @app.route('/')
 def index():
     return render_template("index.html")
-    # weather_data = current_weather()
-    # return weather_data
+
 
